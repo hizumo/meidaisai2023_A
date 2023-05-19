aa078c2e59d730b8213ed182aeb3719f4f985e64
Masato HAYAISHI <cb.90y.9044@s.thers.ac.jp>
2023-05-19 17:22:02 +0900

    17:21　キャラクターの移動を追加

---
+++ Assets/Scripts1/PlayerController1.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController1 : MonoBehaviour
+{
+    // Player1の1マス移動
+    [SerializeField] float moveSpeed;
+
+    bool isMoving;
+    Vector2 input;
+
+    void Update()
+    {
+        // 動いていない時
+        if (!isMoving)
+        {
+            // キーボード入力を受け付ける
+            input.x = Input.GetAxisRaw("Horizontal");
+            input.y = Input.GetAxisRaw("Vertical");
+
+            // 入力があったら
+            if (input != Vector2.zero)
+            {
+                // 入力分を追加
+                Vector2 targetPos = transform.position;
+                targetPos.x += input.x;
+                targetPos.y += input.y;
+                StartCoroutine(Move(targetPos));
+            }
+        }
+    }
+
+    IEnumerator Move(Vector3 targetPos)
+    {
+        isMoving = true;
+
+        // targetPosと現在のpisitionの差がある間は、MoveTowardsでtargetPosに近く
+        while ((targetPos- transform.position).sqrMagnitude > Mathf.Epsilon)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed*Time.deltaTime);
+            yield return null;
+        }
+
+        transform.position = targetPos;
+
+        isMoving = false;
+    }
+}
