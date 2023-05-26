0afa9de7a50e631de66b1d6613545ccb678162cd
koshi <yokota.koshi.g6@s.mail.nagoya-u.ac.jp>
2023-05-26 23:28:45 +0900

    アイテム選択画面にアイテム情報の表示機能を追加

--- Assets/Scripts/PutItemInBackPack.cs
+++ Assets/Scripts/PutItemInBackPack.cs
@@ -8,53 +8,74 @@ public class PutItemInBackPack : MonoBehaviour
     [SerializeField] private ItemDataBase itemDataBase;
 
     [SerializeField] private GameObject itemButton;
-    public GameObject itemInBP;
-    public TextMeshProUGUI itemQTYText;
+    private GameObject itemInBP;　　
+
+    private ItemData myItemData;
+
+    private TextMeshProUGUI itemQTYText;
     private int itemQuantity = 0;
+
+    private GameObject information;
+    private TextMeshProUGUI informationText;
 
     void Start()
     {
+        //　アイテムデータリストを取得
         List<ItemData> myItemDataList = itemDataBase.GetItemDataList();
-    }
-    //　ボタンを押したときの処理
-    public void OnClick()
-    {
         //　どのアイテムボタンか判別
         if (itemButton.name == "ItemR1")
         {
-            Debug.Log(itemButton.name);
+            //Debug.Log(itemButton.name);
             itemInBP = GameObject.Find("ItemSelectUI/Panel/BackPackMain/Item1");
-            //itemNumber = 1;
         }
         else if (itemButton.name == "ItemR2")
         {
-            Debug.Log(itemButton.name);
+            //Debug.Log(itemButton.name);
             itemInBP = GameObject.Find("ItemSelectUI/Panel/BackPackMain/Item2");
-            //itemNumber = 2;
         }
         else if (itemButton.name == "ItemR3")
         {
-            Debug.Log(itemButton.name);
+            //Debug.Log(itemButton.name);
             itemInBP = GameObject.Find("ItemSelectUI/Panel/BackPackMain/Item3");
-            //itemNumber = 3;
         }
         else
         {
             Debug.Log("Error");
             return;
         }
-
         //　対応するアイテムデータを取得
-        ItemData myItemData = itemDataBase.GetItemData(itemInBP.name);
+        myItemData = itemDataBase.GetItemData(itemInBP.name);
+        //　アイテムの個数の表示UIを取得
+        itemQTYText = itemInBP.GetComponentInChildren<TextMeshProUGUI>();
+        //　アイテム情報の表示UIを取得
+        information = GameObject.Find("ItemSelectUI/Panel/Information");
+        informationText = information.GetComponentInChildren<TextMeshProUGUI>();
+    }
+    //　ボタンを押したときの処理
+    public void OnClick()
+    {
         //　アイテムの個数を取得し、＋１する
         itemQuantity = myItemData.GetItemQuantity();
         itemQuantity++;
-        Debug.Log(itemQuantity.ToString());
+        //Debug.Log(itemQuantity.ToString());
         //　個数の変更を記録
         myItemData.ChangeItemQuantity(itemQuantity);
         //　アイテムとその個数を表示
-        itemInBP.SetActive(true);
-        itemQTYText = itemInBP.GetComponentInChildren<TextMeshProUGUI>();
+        if(itemQuantity > 0)
+        {
+            itemInBP.SetActive(true);
+        }
         itemQTYText.text = itemQuantity.ToString();
+    }
+
+    public void MouseOver()
+    {
+        informationText.text = myItemData.GetItemInformation();
+        //Debug.Log("MouseOver");
+    }
+    public void MouseExit()
+    {
+        informationText.text = "";
+        //Debug.Log("MouseExit");
     }
 }
--- Assets/Scripts/ReduceItemQuantityInBP.cs
+++ Assets/Scripts/ReduceItemQuantityInBP.cs
@@ -2,30 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-
+using UnityEngine.UIElements;
 
 public class ReduceItemQuantityInBP : MonoBehaviour
 {
     [SerializeField] private ItemDataBase itemDataBase;
 
     [SerializeField] private GameObject itemInBP;
-    public TextMeshProUGUI itemQTYText;
+
+    private ItemData myItemData;
+
+    private TextMeshProUGUI itemQTYText;
     private int itemQuantity = 0;
+
+    private GameObject information;
+    private TextMeshProUGUI informationText;
+
 
     // Start is called before the first frame update
     void Start()
     {
         List<ItemData> myItemDataList = itemDataBase.GetItemDataList();
+
+        //　対応するアイテムデータを取得
+        myItemData = itemDataBase.GetItemData(itemInBP.name);
+        //　アイテムの個数の表示UIを取得
+        itemQTYText = itemInBP.GetComponentInChildren<TextMeshProUGUI>();
+        //　アイテム情報の表示UIを取得
+        information = GameObject.Find("ItemSelectUI/Panel/Information");
+        informationText = information.GetComponentInChildren<TextMeshProUGUI>();
     }
 
     public void OnClick()
-    {
-        //　対応するアイテムデータを取得
-        ItemData myItemData = itemDataBase.GetItemData(itemInBP.name);
+    {        
         //　アイテムの個数を取得し、ー１する
         itemQuantity = myItemData.GetItemQuantity();
         itemQuantity--;
-        Debug.Log(itemQuantity.ToString());
+        //Debug.Log(itemQuantity.ToString());
         //　個数の変更を記録
         myItemData.ChangeItemQuantity(itemQuantity);
         //　アイテムとその個数を表示
@@ -36,5 +49,15 @@ public class ReduceItemQuantityInBP : MonoBehaviour
         }
         itemQTYText = itemInBP.GetComponentInChildren<TextMeshProUGUI>();
         itemQTYText.text = itemQuantity.ToString();
+    }
+    public void MouseOver()
+    {
+        informationText.text = myItemData.GetItemInformation();
+        //Debug.Log("MouseOver");
+    }
+    public void MouseExit()
+    {
+        informationText.text = "";
+       // Debug.Log("MouseExit");
     }
 }
