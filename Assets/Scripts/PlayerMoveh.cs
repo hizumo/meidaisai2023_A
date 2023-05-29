a788e0db8c253eba879e9fb617fe590c2dca4037
hizumo <104607366+hizumo@users.noreply.github.com>
2023-05-29 20:03:36 +0900

    HPバーの完成

--- Assets/Scripts/PlayerMoveh.cs
+++ Assets/Scripts/PlayerMoveh.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;//UIを扱う際に必要
 
@@ -8,9 +9,18 @@ public class PlayerMoveh : MonoBehaviour
     public Slider slider;
     private float speed = 3.0f;
 
+    //カウントダウン
+    public float countdown = 100.0f;
+
+    //時間を表示するText型の変数
+    [SerializeField] TextMeshProUGUI timeText; //TextMeshProの変数宣言
+
+    //ポーズしているかどうか
+    private bool isPose = false;
+
     void Start()
     {
-        slider.value = 3;
+        slider.value = 100;
     }
 
     void Update()
@@ -22,18 +32,59 @@ public class PlayerMoveh : MonoBehaviour
           transform.position.y,
           transform.position.z + moveZ
         );
+        //クリックされたとき
+        if (Input.GetMouseButtonDown(0))
+        {
+            //ポーズ中にクリックされたとき
+            if (isPose)
+            {
+                //ポーズ状態を解除する
+                isPose = false;
+            }
+            //進行中にクリックされたとき
+            else
+            {
+                //ポーズ状態にする
+                isPose = true;
+            }
+        }
+
+        //ポーズ中かどうか
+        if (isPose)
+        {
+            //ポーズ中であることを表示
+            timeText.text = "ポーズ中";
+
+            //カウントダウンしない
+            return;
+        }
+
+        //時間をカウントする
+        countdown -= Time.deltaTime;
+        //HPバーを動かす
+        slider.value = slider.value - Time.deltaTime;
+
+        //時間を表示する
+        timeText.text = countdown.ToString("f1") + "秒";
+
+        //countdownが0以下になったとき
+        if (countdown <= 0)
+        {
+            timeText.text = "時間になりました！";
+        }
     }
 
     void OnCollisionEnter(Collision other)
     {
         if (other.gameObject.name == "PointDown")
         {
-            slider.value--;
+            slider.value = slider.value - 10.0f;
         }
 
         if (other.gameObject.name == "PointUp")
         {
-            slider.value++;
+            slider.value = slider.value - 3.0f;
         }
+
     }
 }
