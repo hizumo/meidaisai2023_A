583b04e8d2f84fe1141558adbe42b84af49b707b
so-mo-so-mo <rp.94c.7160@s.thers.ac.jp>
2023-06-01 20:03:55 +0900

    BGMとクリックエフェクト（途中）

--- Assets/Scripts/ClickEffect.cs
+++ Assets/Scripts/ClickEffect.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class ClickEffect : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject prefab;
+    public float deleteTime = 1.0f;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            //マウスカーソルの位置を取得。
+            var mousePosition = Input.mousePosition;
+            mousePosition.z = 3f;
+            GameObject clone = Instantiate(prefab, Camera.main.ScreenToWorldPoint(mousePosition),
+                Quaternion.identity);
+            Destroy(clone, deleteTime);
+        }
     }
 }
