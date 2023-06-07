5397aa01c5a18e84a9e9ef5b6f6ae11ec3e1564a
hizumo <104607366+hizumo@users.noreply.github.com>
2023-06-07 16:35:08 +0900

    ゲーム画面からインベントリを開けるように変更

---
+++ Assets/Scripts/InventoryScript.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static PlayerController1;
+
+public class InventoryScript : MonoBehaviour
+{
+    //インベントリパネルの設定
+    [SerializeField] GameObject panelI;
+    [SerializeField] GameObject panelG;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+  
+    }
+    public void InventoryClick()
+    {
+        panelG.SetActive(false);
+        panelI.SetActive(true);
+        return;
+    }
+    public void GameClick()
+    {
+        panelG.SetActive(true);
+        panelI.SetActive(false);
+        return;
+    }
+}
